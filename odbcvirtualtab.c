3d2a1ebda53303ade15b458a230a8e814403a6a9
luohua <luohua>
2025-02-15 17:14:21 +0800

    add compile script

--- odbcvirtualtab.c
+++ odbcvirtualtab.c
@@ -13,13 +13,13 @@
 #if !defined(SQLITEINT_H)
 #include "sqlite3ext.h"
 #endif
-SQLITE_EXTENSION_INIT3
+SQLITE_EXTENSION_INIT1
 #include <string.h>
 #include <assert.h>
-#include "..\..\Sqlite3\clibs\uthash\src\uthash.h"
+#include "..\uthash\src\uthash.h"
 #define XXH_STATIC_LINKING_ONLY /* access advanced declarations */
 #define XXH_IMPLEMENTATION      /* access definitions */
-#include "..\..\Sqlite3\clibs\xxHash\xxhash.h"
+#include "..\xxHash\xxhash.h"
 #undef XXH_STATIC_LINKING_ONLY
 #undef XXH_IMPLEMENTATION
 struct clientConnectionhandle {
